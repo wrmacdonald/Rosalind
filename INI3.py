88c6242af1a7c634dab75c0f08dafd9843884355
wrmacdonald <wrmacdonald2016@gmail.com>
2020-07-26 20:08:53 -0700

    initial build

---
+++ INI3.py
@@ -0,0 +1,8 @@
+string = "biefcSid326jV7RsXNC20Jfq1H7bwjdf7P6CalotesLRUPH9snALNQDaXIc8BXjhlOSiuLhEkcT6Fyyoo88YUdcarinataeCSDKGKLG5xO8OAtYskkM7yRUEXvooY67NBbazVo0a5SYczA2jRm3GP9QmN5BtacDOymfE7elW1F22O."
+a = 35
+b = 41
+c = 86
+d = 93
+a_b = string[a:b+1]
+c_d = string[c:d+1]
+print(f"{a_b} {c_d}")
