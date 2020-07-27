44b1c48b730a705a7f1e8666cb3a39d543baf05a
wrmacdonald <wrmacdonald2016@gmail.com>
2020-07-27 10:13:39 -0700

    change to read in file

--- INI3.py
+++ INI3.py
@@ -1,8 +1,13 @@
-string = "biefcSid326jV7RsXNC20Jfq1H7bwjdf7P6CalotesLRUPH9snALNQDaXIc8BXjhlOSiuLhEkcT6Fyyoo88YUdcarinataeCSDKGKLG5xO8OAtYskkM7yRUEXvooY67NBbazVo0a5SYczA2jRm3GP9QmN5BtacDOymfE7elW1F22O."
-a = 35
-b = 41
-c = 86
-d = 93
+with open("/Users/wesmacdonald/Downloads/rosalind_ini3.txt") as file:
+    string = file.readline()
+    abcd = file.readline()
+abcd_list = abcd.split()
+
+a = int(abcd_list[0])
+b = int(abcd_list[1])
+c = int(abcd_list[2])
+d = int(abcd_list[3])
+
 a_b = string[a:b+1]
 c_d = string[c:d+1]
 print(f"{a_b} {c_d}")
